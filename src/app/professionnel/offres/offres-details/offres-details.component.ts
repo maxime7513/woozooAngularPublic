8206c8f9a8d132b57111eb73e1bb776558debdf3
maxime <maxbln7513@gmail.com>
2022-01-10 19:36:43 +0100

    rought notation offres-details

--- src/app/professionnel/offres/offres-details/offres-details.component.ts
+++ src/app/professionnel/offres/offres-details/offres-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { annotate, annotationGroup } from 'rough-notation';
 
 @Component({
   selector: 'app-offres-details',
@@ -9,6 +10,7 @@ export class OffresDetailsComponent implements OnInit {
   contenuVisible : string;
   scrolled: boolean = false;
   scrolledDepasse: boolean = false;
+  notationOneTime: boolean = false;
   @HostListener("window:scroll", [])
   onWindowScroll() { // classe active sur header au scroll
       let offreDetails = document.getElementById("offre_details") as HTMLCanvasElement,
@@ -22,9 +24,7 @@ export class OffresDetailsComponent implements OnInit {
     this.contenuVisible = 'securite';
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {} 
   // scroll(el: HTMLElement){ // function scroll
   //   let y = el.offsetTop;
   //   window.scrollTo({top: y - 64, behavior: 'smooth'});
@@ -34,5 +34,24 @@ export class OffresDetailsComponent implements OnInit {
     this.contenuVisible = el;
     let offre = document.getElementById("offre_details") as HTMLCanvasElement;
     window.scrollTo({top: offre.offsetTop - 64, behavior: 'smooth'});
+    
+    if(el == 'securite' && this.notationOneTime == false){ // lancer fonction roughtNotation() si categorie securite et fonction pas encore effectué
+      setTimeout(() => {
+        this.roughtNotation();
+      }, 1000);
+    }else if(el != 'securite'){
+      this.notationOneTime = false; // remettre variable à false pour pouvoir relancer la fonction roughtNotation() 
+    }
+  }
+
+  roughtNotation(){
+    const a1 = annotate(document.getElementById('scelle') as HTMLElement, { type: 'box', color: 'var(--blue-secondary)', padding: [1, 3] });
+    const a2 = annotate(document.getElementById('qrcode') as HTMLElement, { type: 'box', color: 'var(--blue-secondary)', padding: [1, 3] });
+    const a3 = annotate(document.getElementById('code4') as HTMLElement, { type: 'box', color: '#0eb3b7', padding: [1, 3] });
+    const a4 = annotate(document.getElementById('remp') as HTMLElement, { type: 'underline', color: 'var(--blue-secondary)', padding: 2 });
+    const a5 = annotate(document.getElementById('dixm') as HTMLElement, { type: 'circle', color: '#0eb3b7', padding: 8, animationDuration: 1000});
+    const agroupe = annotationGroup([a1, a2, a3, a4, a5]);
+    agroupe.show();
+    this.notationOneTime = true; // la fonction est effectué et ne peut pas etre relancé
   }
 }
