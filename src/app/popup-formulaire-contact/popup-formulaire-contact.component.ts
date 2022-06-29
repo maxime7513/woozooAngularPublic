95383cc5c7b2b2ae0759833fd813c127cee36966
maxime7513 <makitoo@outlook.fr>
2022-06-29 16:38:06 +0200

    form contact

--- src/app/popup-formulaire-contact/popup-formulaire-contact.component.ts
+++ src/app/popup-formulaire-contact/popup-formulaire-contact.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faPhoneSquareAlt, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { HotToastService } from '@ngneat/hot-toast';
+import { EmailService } from '../services/email.service';
 
 
 @Component({
@@ -10,10 +13,70 @@ import { faPhoneSquareAlt, faPaperPlane} from '@fortawesome/free-solid-svg-icons
 export class PopupFormulaireContactComponent implements OnInit {
   phoneIcon = faPhoneSquareAlt;
   paperPlaneIcon = faPaperPlane;
+  contactForm: FormGroup = new FormGroup(
+    {
+      nom: new FormControl('', Validators.required),
+      raisonSociale: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      message: new FormControl("", Validators.required),
+    }
+  );
 
-  constructor() { }
+  constructor(private emailService: EmailService, private toast: HotToastService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  // getter for error
+  get nom() {
+    return this.contactForm.get('nom');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  submit(){
+    this.toast.close();
+
+    if(this.nom?.hasError('required')){
+      this.toast.error('Veuillez renseigner un nom')
+    }else if(this.email?.hasError('required')){
+      this.toast.error('Veuillez renseigner un email')
+    }else if(this.email?.hasError('email')){
+      this.toast.error('Veuillez renseigner un email valide')
+    }else if(this.message?.hasError('required')){
+      this.toast.error('Veuillez saisir un message')
+    }
+
+    if (!this.contactForm.valid) {
+      return;
+    }
+
+    const toastValid = this.toast.loading('envoie en cours...',{duration: 2500});
+
+    const {nom, raisonSociale, phone, email, message} = this.contactForm.value;
+    const html = `<b>
+                    <p style="color: #046497"; margin: 5px 0;><span style="color: #7e8da9;">Nom: </span>`+ nom +`</p>
+                    <p style="color: #046497"; margin: 5px 0;><span style="color: #7e8da9;">Raison sociale: </span>` + raisonSociale + `</p>
+                    <p style="color: #046497"; margin: 5px 0;><span style="color: #7e8da9;">Telephone: </span>` + phone + `</p>
+                    <p style="color: #046497"; margin: 5px 0;><span style="color: #7e8da9;">Email: </span>` + email + `</p>
+                    <p style="color: #046497"; margin: 5px 0;><span style="color: #7e8da9;">Message: </span><br>` + message + `</p>
+                  </b>`;
+    
+    let req = {
+      to: 'blnmax@yahoo.com',
+      subject: 'Message provenant du site vitrine',
+      html: html
+    }
+
+    this.emailService.send_mail(req);
+
+    toastValid.afterClosed.subscribe((e) => {
+      this.toast.success('Formulaire envoyé avec succès')
+    });
   }
 
 }
