cad35ea71ad31644aee629b9b2f694dac30cc3c1
maxime7513 <makitoo@outlook.fr>
2022-06-29 16:36:20 +0200

    add email service

---
+++ src/app/services/email.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  private header = { headers: { "Content-Type": "application/json" } };
+
+  constructor(private http: HttpClient) { }
+
+  send_mail(req: any) {
+    return this.http.post("http://localhost:3000/sendMail", req , this.header).subscribe((resp: any) => {
+    // return this.http.post("https://limitless-earth-25794.herokuapp.com/sendMail", req , this.header).subscribe((resp: any) => {
+      console.log(resp);
+    });
+  }
+
+}
