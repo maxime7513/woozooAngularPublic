cc1e8e07e4856730c2d5d283aeddaf4a510c4e7c
maxime7513 <makitoo@outlook.fr>
2022-07-01 01:06:44 +0200

    responsive

--- src/app/professionnel/presentation/presentation.component.ts
+++ src/app/professionnel/presentation/presentation.component.ts
@@ -13,23 +13,38 @@ export class PresentationComponent implements OnInit {
   tabSliderPresentation : Array<string>;
   iSlider : any;
   commentaires: Array<{nom: string, enseigne: string, img: string, texte: string}>
-  slideConfig = {"slidesToShow": 3, "slidesToScroll": 1, "autoplay": true};
+  getScreenWidth: any = window.innerWidth;
+  slidesToShow: number;
+  slideConfig: any;
+  imgBox: string;
 
   constructor(private data: CommentairesService) {
     this.dotNav = 'presentation1';
     this.tabSliderPresentation= ["presentation1", "presentation2", "presentation3"];
     this.iSlider = 1;
     this.commentaires = this.data.clients;
+    
+    if(this.getScreenWidth > 800 && this.getScreenWidth <= 1250){
+      this.slidesToShow = 1;
+    }else{
+      this.slidesToShow = 3;
+    }
+    this.slideConfig = {"slidesToShow": this.slidesToShow, "slidesToScroll": 1, "autoplay": true};
+    if(this.getScreenWidth > 800){
+      this.imgBox = 'mockup-box';
+    }else{
+      this.imgBox = 'mockup-box_mobile';
+    }
   }
 
   ngOnInit(): void {
-    setInterval(() => { // slider presentation
-      this.dotNav = this.tabSliderPresentation[this.iSlider];
-      this.iSlider++;
-      if(this.iSlider >= this.tabSliderPresentation.length){
-        this.iSlider = 0;
-      }
-    }, 5000);
+    // setInterval(() => { // slider presentation
+    //   this.dotNav = this.tabSliderPresentation[this.iSlider];
+    //   this.iSlider++;
+    //   if(this.iSlider >= this.tabSliderPresentation.length){
+    //     this.iSlider = 0;
+    //   }
+    // }, 5000);
   }
 
 }
