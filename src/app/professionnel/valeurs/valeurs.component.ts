e5785dc8831f442b0ccbeb337e466585b59fcc68
maxime7513 <makitoo@outlook.fr>
2022-07-02 03:55:48 +0200

    valeurs slider responsive

--- src/app/professionnel/valeurs/valeurs.component.ts
+++ src/app/professionnel/valeurs/valeurs.component.ts
@@ -6,6 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./valeurs.component.scss']
 })
 export class ValeursComponent implements OnInit {
+  valeurs: Array<{img: string, titre: string, texte: string}> = [
+    {img: "securite",
+    titre: "Sécurité",
+    texte: "Nous vous garantissons une livraison 100% sécurisée, grâce à un process établie",
+    },
+    {img: "qualite",
+    titre: "Qualité",
+    texte: "Nous formons nos coursiers partenaires pour vous assurer une livraison de qualité",
+    },
+    {img: "urgence",
+    titre: "Urgence",
+    texte: "La réactivité de notre service, vous garantie une livraison toujours plus rapide",
+    },
+    {img: "equitable",
+    titre: "Equité",
+    texte: "Le bien être et la valorisation de nos coursier sont au coeur de nos préoccupations",
+    }
+  ];
+  
+  slideConfig = {
+    "slidesToShow": 4,
+    "slidesToScroll": 1,
+    "autoplay": true,
+    responsive: [
+      {
+        breakpoint: 551,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 999,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+    ]
+  };
 
   constructor() { }
 
