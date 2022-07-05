2d484cabedac7a7288cbf0a9bfa363259f5a28f8
maxime7513 <makitoo@outlook.fr>
2022-07-05 23:51:21 +0200

    google recaptcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  recaptcha: {
+    siteKey: '6LeL4cggAAAAAAQMKaESmn3slvTxmO-Q1CY42eMO',
+  },  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  recaptcha: {
+    siteKey: '6LeL4cggAAAAAAQMKaESmn3slvTxmO-Q1CY42eMO',
+  },  
 };
 
 /*
