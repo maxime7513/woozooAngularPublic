5016cb635679d248204587c570887a697eaf209a
maxime7513 <makitoo@outlook.fr>
2022-07-16 21:12:47 +0200

    seo balise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,58 @@ import { ConfidentialiteComponent } from './confidentialite/confidentialite.comp
 import { ConditionsGeneralesComponent } from './conditions-generales/conditions-generales.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent , data: {animation: 'Home'} },
-  { path: 'professionnel', component: ProfessionnelComponent , data: {animation: 'Professionnel'} },
-  { path: 'secteur', component: SecteurComponent , data: {animation: 'Secteur'} },
-  { path: 'coursier', component: CoursierComponent , data: {animation: 'Coursier'} },
+  { path: '', component: HomePageComponent ,
+    data: {
+      animation: 'Home',
+      seo: {
+        title: 'Woozoo | Solution de livraison rapide, flexible et sécurisée',
+        metaTags: [
+          { name: 'description', content: 'Woozo est la solution de livraison qui vous connecte à la meilleur flotte de livreurs de votre ville.' },
+          { property: 'og:title', content: 'Woozoo | Solution de livraison rapide, flexible et sécurisée' },
+          { proprety: 'og:description', content: 'Woozo est la solution de livraison qui vous connecte à la meilleur flotte de livreurs de votre ville.' },
+        ]
+      }
+    }
+  },
+  { path: 'professionnel', component: ProfessionnelComponent ,
+    data: {
+      animation: 'Professionnel',
+      seo: {
+        title: 'Livraison pour tous les commerces et entreprises',
+        metaTags: [
+          { name: 'description', content: 'Woozo est la solution de livraison qui vous connecte à la meilleur flotte de livreurs de votre ville.' },
+          { property: 'og:title', content: 'Woozoo | Livraison pour tous les commerces et entreprises' },
+          { proprety: 'og:description', content: 'Woozo est la solution de livraison qui vous connecte à la meilleur flotte de livreurs de votre ville.' },
+        ]
+      }
+    } 
+  },
+  { path: 'secteur', component: SecteurComponent ,
+    data: {
+      animation: 'Secteur',
+      seo: {
+        title: "Nos secteurs d'activité",
+        metaTags: [
+          { name: 'description', content: 'Woozo est la solution de livraison instantané pour tous les commerces et entreprises.' },
+          { property: 'og:title', content: "Woozoo | Nos secteurs d'activité" },
+          { proprety: 'og:description', content: 'Woozo est la solution de livraison instantané pour tous les commerces et entreprises.' },
+        ]
+      }
+    }
+  },
+  { path: 'coursier', component: CoursierComponent ,
+    data: {
+      animation: 'Coursier',
+      seo: {
+        title: "Devenez coursier partenaire Woozoo",
+        metaTags: [
+          { name: 'description', content: "Rouler avec woozoo, c'est la liberté de livrer où vous voulez et quand vous voulez. Rejoins-nous dès maintenant!" },
+          { property: 'og:title', content: 'Devenez coursier partenaire Woozoo' },
+          { proprety: 'og:description', content: "Rouler avec woozoo, c'est la liberté de livrer où vous voulez et quand vous voulez. Rejoins-nous dès maintenant!" },
+        ]
+      }
+    }
+  },
   { path: 'mentionsLegales', component: MentionsLegalesComponent , data: {animation: 'MentionsLegales'} },
   { path: 'confidentialite', component: ConfidentialiteComponent , data: {animation: 'Confidentialite'}},
   { path: 'conditionsGenerales', component: ConditionsGeneralesComponent , data: {animation: 'ConditionsGenerales'}},
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './route-animation';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { SEOService } from './services/seo.service';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +12,30 @@ import { slideInAnimation } from './route-animation';
   animations: [ slideInAnimation ]
 })
 export class AppComponent {
-  title = 'woozooAngular';
+  // title = 'woozooAngular';
+  title = 'got-prerender-demo';
+
   prepareRoute(outlet: RouterOutlet) { // animation router
     return outlet?.activatedRouteData?.['animation'];
+  }
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private seoService: SEOService){}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data),
+    ).subscribe(data => {
+      let seoData = data['seo'];
+      this.seoService.updateTitle(seoData['title']);
+      this.seoService.updateMetaTags(seoData['metaTags']);
+    });
   }
 }
 
